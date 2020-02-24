1f720efad02dd30231691f328ee61565b9705b80
Owais Alam <ovais.alam@sixlogs.com>
2020-02-24 17:42:35 +0500

    code cleaning

--- src/App.js
+++ src/App.js
@@ -6,42 +6,9 @@ import './App.css';
 
 function App(props) {
 
-// ReactDOM.render(element,document.getElementById('root'))
+ReactDOM.render(element,document.getElementById('root'))
 }
 
-class Clock extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {date:new Date()}
-  }
-
-  componentDidMount(){
-    this.timerId = setInterval(
-      () => {
-        this.setState({date:new Date()})
-      },
-      1000
-      );
-  }
-
-  componentWillUnmount(){
-    clearInterval(this.timerId)
-  }
-   
-  // tick(){
-  //   this.setState({date:new Date()}) 
-  // }
-  
-  render(){
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-    return element
-  }
-}
 
 
 export default Clock   ;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Clock from './App';
+import Clock from './components/Clock';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Clock/>,document.getElementById('root'))
